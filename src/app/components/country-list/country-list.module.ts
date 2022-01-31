d9b3db54dc15a4d1fef8a0524252576fa28dccad
Pratik Sharma <pratiksharma@live.in>
2022-01-31 14:42:10 +0530

    Added Animations

--- src/app/components/country-list/country-list.module.ts
+++ src/app/components/country-list/country-list.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { CountryListComponent } from "./country-list";
-import { CountryCardComponent } from "../country-card/country-card.component";
-import { SharedModule } from "../shared/shared.module";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CountryListComponent } from './country-list';
+import { CountryCardComponent } from '../country-card/country-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
--- src/app/components/country-list/country-list.ts
+++ src/app/components/country-list/country-list.ts
@@ -1,11 +1,14 @@
+
 import { Component, Input, OnInit } from '@angular/core';
 import { CountriesService } from 'src/app/core/countries/countries.service';
+import { fadeAnimation } from '../shared/animation/fade.animation';
 import { Country } from '../shared/types';
 
 @Component({
   selector: 'ci-country-list',
   templateUrl: './country-list.html',
   styleUrls: ['./country-list.scss'],
+  animations: [fadeAnimation],
 })
 export class CountryListComponent implements OnInit {
   public countryList: Country[] = [];
@@ -13,14 +16,14 @@ export class CountryListComponent implements OnInit {
 
   @Input() public searchText = '';
 
-  constructor(
-    private countriesService: CountriesService
-  ) {}
+  constructor(private countriesService: CountriesService) {}
 
   ngOnInit(): void {
-    this.countriesService.getAllCountries().subscribe((countries: Country[]) => {
-      this.countryList = countries;
-      this.isloading = false;
-    });
+    this.countriesService
+      .getAllCountries()
+      .subscribe((countries: Country[]) => {
+        this.countryList = countries;
+        this.isloading = false;
+      });
   }
 }
